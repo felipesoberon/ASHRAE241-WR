998a8133dc675c3c0c88dd53a38333bc93501ef9
Felipe Soberon <felipe.soberon@gmail.com>
2025-07-13 18:39:37 +0100

    updated streamlit GUI to have ECAi values from 241 for Probability Calculation

--- streamlitGUI.py
+++ streamlitGUI.py
@@ -12,7 +12,12 @@ categories = list(occupancy_params.keys())
 # Sidebar for user inputs
 st.sidebar.header("Simulation Settings")
 calculation_type = st.sidebar.selectbox("Calculation Type:", ["ECAi", "Infection Probability"])
-use_ashrae_ecai = st.sidebar.checkbox("Use ASHRAE 241 ECAi values", value=True)
+
+#use_ashrae_ecai = st.sidebar.checkbox("Use ASHRAE 241 ECAi values", value=True)
+st.sidebar.markdown("### Use ASHRAE 241 ECAi values")
+st.sidebar.markdown("**(For Probability Calculation)**")
+use_ashrae_ecai = True
+
 allow_zero_infector = st.sidebar.checkbox("Allow Zero Infector Simulations", value=True)
 use_ashrae_cir = st.sidebar.checkbox("Use ASHRAE 241 Community Infection Rate", value=True)
 
